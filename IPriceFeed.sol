aefc6afa637fa5259cfa685337012e45163128f8
FriedrichMuelle <141995328+FriedrichMuelle@users.noreply.github.com>
2024-04-29 15:06:41 +0800

    Delete IPriceFeed.sol

--- IPriceFeed.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-interface IPriceFeed {
-
-    function getPrice(address _token, bool _maximise, bool _fresh) external view returns (uint256);
-    function getUpdateFee(bytes[] calldata _updateData) external view returns (uint256);
-    function updatePriceFeeds(bytes[] calldata _priceData) external payable;
-}
